4a04f485ee1509bd6eb806af38372820d914c873
DweejaReddy <74244501+DweejaReddy@users.noreply.github.com>
2023-05-27 18:11:53 +0000

    added dynamic data with list of dict

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location':'Bangalore, India',
+    'salary':'10,00,000'
+  },
+  {
+    'id':2,
+    'title':'Web Developer',
+    'location':'Pune, India',
+    'salary':'20,00,000'
+  },
+  {
+    'id':3,
+    'title':'Data Scientist',
+    'location':'Mumbai, India'
+  }
+]
 @app.route("/")
 
 def hello_world():
-  return "Hello, World!!"
+  return render_template('home.html', jobs = JOBS,
+                        company_name = 'Dweeja')
+
+
+@app.route("/api/jobs")
+
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
